9b52396886f86385c671e8b4f01c482c311480a8
edikustriyadi21@gmail.com <edikustriyadi21@gmail.com>
2025-04-16 10:31:30 +0700

    setup docker, mysql

---
+++ src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<h1 class="text-red-500">Welcome to SvelteKit</h1>
+<p class="border border-red-200">
+	Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
+</p>
